8841a8afa75581b4d3f59bfec3f7b126bd2b4621
vt4a2h <vt4a2h@gmail.com>
2014-10-13 15:58:28 +0700

    finish testing

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -1,3 +1,27 @@
 pub fn add_three_times_four(x: int) -> int {
-	(x + 3) * 4
+	times_four(add_three(x))
+}
+
+fn add_three(x: int) -> int { x + 3 }
+
+fn times_four(x: int) -> int { x * 4 }
+
+#[cfg(test)]
+mod test {
+	use super::add_three;
+	use super::times_four;
+
+	#[test]
+	fn test_add_three() {
+		let result = add_three(5i);
+
+		assert_eq!(8i, result);
+	}
+
+	#[test]
+	fn test_times_four() {
+		let result = times_four(5i);
+
+		assert_eq!(20i, result);
+	}
 }
