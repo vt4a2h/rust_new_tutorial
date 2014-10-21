d8001479a40c228b6e7574086d72148365697ea9
vt4a2h <vt4a2h@gmail.com>
2014-10-21 15:44:32 +0700

    closures ext

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -7,7 +7,14 @@ fn main() {
     let printer = || { println!("x is {}", x); }; // x borrowed now
     printer();
 
-    let y = 4i;
     let p = proc() { x * x }; // can be called once
     println!("{}", p());
+
+    let square = |x: int| { x * x };
+    let result = twice(5i, square);
+    println!("{}", result);
+}
+
+fn twice(x: int, f: |int| -> int) -> int {
+	f(x) + f(x)
 }
