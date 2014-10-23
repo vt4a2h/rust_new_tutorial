61afa87ba35750aec617a9193587a17127ee36b8
vt4a2h <vt4a2h@gmail.com>
2014-10-23 15:53:06 +0700

    adapters

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -26,4 +26,26 @@ fn main() {
 
     let sum = range(1i, 4i).fold(0i, |sum, x| sum + x);
     println!("{}", sum);
+  
+    // iterators are lazy
+    range(1i, 100i).map(|x| x + 1i);            // no effect and warning
+    range(1i, 100i).map(|x| println!("{}", x)); // the same with previous
+
+    for i in std::iter::count(0i, 1i).take(5) {
+    	println!("{}", i);
+    }
+
+    for i in range(1i, 100i).filter(|x| x % 2 == 0) {
+    	println!("{}", i);
+    }
+
+    let ve1 = range(1i, 1000i)
+              .filter(|x| x % 2 == 0)
+              .filter(|x| x % 3 == 0)
+              .take(5)
+              .collect::<Vec<int>>();
+
+    for element in ve1.iter() {
+    	println!("{}", element);
+    }
 }
