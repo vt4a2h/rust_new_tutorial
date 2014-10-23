dd2a72d9da08797d4b8256557aab2ec72d2506a3
vt4a2h <vt4a2h@gmail.com>
2014-10-23 15:16:57 +0700

    fix old code. extended consumers

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let mut range = range(0i, 10i);
+    let mut range1 = range(0i, 10i);
 
     loop {
-    	match range.next() {
+    	match range1.next() {
     		Some(x) => {
     			println!("{}", x);
     		},
@@ -15,10 +15,15 @@ fn main() {
     	println!("{}", *num);
     }
 
+    let one_two = range(1i, 2i).collect::<Vec<int>>();
+
     // consumers
     let greatter_than_forty_two = range(0i, 100i).find(|x| *x >= 42);
     match greatter_than_forty_two {
     	Some(_) => println!("We got some numbers!"),
     	None    => println!("No numbers found :("),
     }
+
+    let sum = range(1i, 4i).fold(0i, |sum, x| sum + x);
+    println!("{}", sum);
 }
